d0de91e6f246b1ff9755c68d8cee7782c48721c1
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-12-18 09:25:18 +0100

    sidste kommentarer til mediaID klassen tilføjet

--- src/MediaID.java
+++ src/MediaID.java
@@ -14,29 +14,36 @@ public class MediaID {
     private static int latestId = 100;
 
 
-    // her oprettes en metode som opretter en ny text fil
+    // her oprettes en metode som opretter en ny text fil og returnerer en int
     public static int generate() {
 
+        // her oprettes en ny "File" og så kaldes text dokumentet media_id.txt
         File file = new File("media_id.txt");
 
         // Låser filen op før adgang
         // file.setWritable(true);
 
+        // her laves en try catch block som fanger exceptions i koden kører programmet selvom fejl opstår
         try {
 
 
+            // dette stykke kode stopper filer fra at blive oprettet med samme navn
+            // og bruger en scanner til at modtage en ny int som ID
             if (file.exists()) {
                 FileReader fileReader = new FileReader(file);
                 Scanner scanner = new Scanner(file);
                 latestId = (int) scanner.nextInt();
             }
 
+            // her incrementes latestId
             latestId++;
 
+            // her oprettes et nyt FileWriter objekt og ID'et laves til en string
             FileWriter fileWriter = new FileWriter(file);
             fileWriter.write(Integer.toString(latestId));
             fileWriter.close();
 
+            // her defineres det hvilke exceptions skal fanges (alle i dette tilfælde)
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,6 +51,7 @@ public class MediaID {
         // Låser filen ned for at den ikke redigeres ved en fejl
         // file.setReadOnly();
 
+        // her returneres ID'et
         return latestId;
     }
 
