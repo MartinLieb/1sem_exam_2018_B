26e37ea310d7f9979465e7611b5c3722eaa3fd64
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-12-18 09:51:09 +0100

    klassediagram2 og nedarvningsklasser tilføjet

---
+++ src/Artikel.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Artikel extends Media {
+
+    private String author;
+    private String articleText;
+    private File picture;
+}
---
+++ src/Video.java
@@ -0,0 +1,8 @@
+public class Video extends Media {
+
+    private String fileType;
+    private String videoLenght;
+    private String resolution;
+    private String photographer;
+
+}
